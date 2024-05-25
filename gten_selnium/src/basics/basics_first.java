9ffdfd221723698d4a2ef6188dcee2f249b9f578
Shubhamsaxena730 <165132617+Shubhamsaxena730@users.noreply.github.com>
2024-05-25 11:10:50 +0530

    Add files via upload
    added some practice  files

---
+++ gten_selnium/src/basics/basics_first.java
@@ -0,0 +1,45 @@
+package basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class basics_first {
+
+
+	public static void  main (String[] args ) throws InterruptedException {
+		/**
+		 * Ctrl+shift+o -> bring appropriate imports
+		 */
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		//opens a url
+		driver.get("https:demoblaze.com/");
+		
+		// grab a title
+		String title = driver.getTitle();
+		
+		System.out.println(title);
+		
+		driver.navigate().back();
+		
+		Thread.sleep(2000);
+		
+		driver.navigate().forward();
+		
+		 System.out.println( driver.getCurrentUrl());
+		 
+		 Thread.sleep(2000);
+		
+		 //will always close the current window/ tab
+		 driver.close();
+		 
+		//will close entire browser
+		driver.quit();
+		
+		
+	}
+}
+		
+		
+	
